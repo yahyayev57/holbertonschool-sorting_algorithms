ff500c6286cf81ae2f9100b336d28babb0993032
Kenan <yahyayevkenan57@gmail.com>
2025-08-13 08:21:12 +0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -4,7 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
+/**
+ * struct listint_s - doubly linked list node
+ * @n: integer stored in the node
+ * @prev: points to the previous element of the list
+ * @next: points to the next element of the list
+ *
+ * Description: doubly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
