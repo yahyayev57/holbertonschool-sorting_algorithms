c34a0ead19d108222f0a21cfcc6842eacf637b7e
Kenan <yahyayevkenan57@gmail.com>
2025-08-13 08:29:14 +0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -23,5 +23,7 @@ void insertion_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 
+void print_array(const int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
